784469cfe525df47b1651d4717eb1af19712a4d1
german <germanoctako@gmail.com>
2024-12-01 13:51:24 +0100

    server

--- calls.py
+++ calls.py
@@ -1,11 +1,11 @@
 from requests import post
 
 record_file = open("recorded_packages.txt", "w")
-with open("all_pypi_packages/all_pypi_packages_0.txt", "r") as file:
+with open("all_pypi_packages/all_pypi_packages_1.txt", "r") as file:
     text = file.read()
     packages = text.split("\n")
 
     for package in packages:
         print("Iniciando extracción para el paquete: ", package)
-        record_file.write(package)
+        record_file.write(package + "\n")
         post(f"http://localhost:8000/graph/pypi/package/init?package_name={package}")
